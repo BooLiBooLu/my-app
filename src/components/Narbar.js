4518865f3ea7690347b742f9099d30bfe6d942a2
AshleyChu <sonic20526@gmail.com>
2021-01-13 16:21:27 +0800

    add <div> in Narbar.js

---
+++ src/components/Narbar.js
@@ -0,0 +1,24 @@
+//rfce
+import React from 'react'
+
+function Narbar() {
+    return (
+        <>
+            <div className="narbar">
+                <div className="narbar-container container">
+                    /**Link */
+                    <Link to='/' className="narbar-logo">
+                        LAVISH
+                    </Link>
+                    <div className="menu-icon">
+
+                    </div>
+                </div>
+
+            </div>
+                
+        </>
+    )
+}
+
+export default Narbar
